87262e9317d9a6e47d816da277fd6cdb7cdb9bbb
LuckyOwlBen <benmalmberg2@gmail.com>
2019-06-21 15:33:52 -0400

    updated for hector

--- WeekBExample/src/com/revature/datastructures/MyLinkedList.java
+++ WeekBExample/src/com/revature/datastructures/MyLinkedList.java
@@ -34,14 +34,14 @@ public class MyLinkedList<T> {
 
 	// remove and return the first item in the list
 	public T poll() {
-		
+
 		Node<T> firstNode = this.head;
-		if(firstNode != null) {
+		if (firstNode != null) {
 			this.head = firstNode.getNextNode();
 			return this.head.getData();
 		}
 		return null;
-		
+
 	}
 
 	// return but do not remove the first item
@@ -49,44 +49,43 @@ public class MyLinkedList<T> {
 
 		if (this.head != null) {
 			return this.head.getData();
-		}
-		else {
+		} else {
 			return null;
 		}
 	}
 
 	// remove the first node whose data matches provided key
 	public boolean removeByKey(T key) {
-		if(key == null) {
+		if (key == null) {
 			return false;
 		}
-		
-		//create a reference to current node
+
+		// create a reference to current node
 		Node<T> currentNode = this.head;
-		
-		//create a reference to the next node(after the current)
+
+		// create a reference to the next node(after the current)
 		Node<T> nextNode = currentNode.getNextNode();
-		
-		//if first node matches the key, move the head to the next node
-		if(currentNode != null && currentNode.getData().equals(key)) {
+
+		// if first node matches the key, move the head to the next node
+		if (currentNode != null && currentNode.getData().equals(key)) {
 			this.head = nextNode;
 			return true;
 		}
-		
-		//iterate through list while currentNode is not null
-		while(currentNode != null) {
-			//if next node is not null and matches the key
-			//point currentNode to the node after nextNode and return true
-			if(nextNode != null && nextNode.getData().equals(key)) {
+
+		// iterate through list while currentNode is not null
+		while (currentNode != null) {
+			// if next node is not null and matches the key
+			// point currentNode to the node after nextNode and return true
+			if (nextNode != null && nextNode.getData().equals(key)) {
 				currentNode.setNextNode(nextNode.getNextNode());
 				return true;
 			}
-			
-			//advance current node to next node of list
+
+			// advance current node to next node of list
 			currentNode = currentNode.getNextNode();
-			
-			//if nextNode is not null advance the next node to the one after it
-			if(nextNode != null) {
+
+			// if nextNode is not null advance the next node to the one after it
+			if (nextNode != null) {
 				nextNode = nextNode.getNextNode();
 			}
 		}
@@ -99,6 +98,44 @@ public class MyLinkedList<T> {
 		while (currentNode != null) {
 			System.out.println("Node location: " + currentNode.hashCode() + ",Node Value: " + currentNode.getData());
 			currentNode = currentNode.getNextNode();
+		}
+	}
+
+	// remove all duplicates from the list
+	public void removeDuplicates() {
+
+		Node<T> currentNode = this.head;
+		Node<T> runner;
+		T currentValue = null;
+		if (this.head != null) {
+			currentValue = currentNode.getData();
+			System.out.println("Top of the list, initial value = " + currentValue);
+			do {
+				System.out.println("at beginning of list, initial value = "+currentValue);
+				runner = currentNode;	//have "runner" start in the same spot as the current node
+				//don't need to re-check earlier nodes in the list
+				while (runner != null && runner.getNextNode() != null ) {
+					T nextValue = runner.getNextNode().getData();
+					System.out.println("Comparing Values ("+currentValue+" ,"+nextValue+")");
+					if (currentValue.equals(nextValue)) {
+						System.out.println("removing first instance of the value: "+currentValue);
+						removeByKey(currentValue);
+					}
+					System.out.println("Moving runner to next node");
+					runner = runner.getNextNode();
+				}
+				//moving currentNode to next node
+				if(currentNode.getNextNode() != null) {
+					System.out.println("no more duplicates of: "+currentValue+" left");
+					currentValue = currentNode.getNextNode().getData();
+					currentNode = currentNode.getNextNode();
+				}
+
+			} while (currentValue != null && currentNode.getNextNode() != null);
+			// exit iteration if current node's value is null or there is no exit node
+			// a refinement would be to scrub out or skip all nodes with null values instead
+			// of rage quitting if we find one
+
 		}
 	}
 }
--- WeekBExample/src/com/revature/datastructures/MyLinkedListDemo.java
+++ WeekBExample/src/com/revature/datastructures/MyLinkedListDemo.java
@@ -9,6 +9,7 @@ public class MyLinkedListDemo {
 		User u1 = new User("Gandalf", "Grey", "mithrandir", "you_shall_not");
 		User u2 = new User("Paul", "Hewson", "bono", "u2RuleZ");
 		User u3 = new User("David", "Tennant", "doc10", "tardis");
+		User u4 = new User("Paul", "Hewson", "bono", "u2RuleZ");
 		
 		MyLinkedList<User> userList = new MyLinkedList<>();
 		
@@ -18,7 +19,8 @@ public class MyLinkedListDemo {
 		
 		userList.insert(u1);
 		userList.insert(u2);
-		userList.insert(u3);		
+		userList.insert(u3);
+		userList.insert(u4);
 		
 		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		System.out.println("List after Insertion");
@@ -30,7 +32,7 @@ public class MyLinkedListDemo {
 		System.out.println("Test Peek");
 		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 	
-		System.out.println("Head : " +userList.peek());
+	/*	System.out.println("Head : " +userList.peek());
 		
 		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		System.out.println("Test poll");
@@ -44,6 +46,9 @@ public class MyLinkedListDemo {
 		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		
 		userList.removeByKey(u3);
+		userList.printList();*/
+		
+		userList.removeDuplicates();
 		userList.printList();
 	}
 
