fc6d2345bfbf91e069a7746f7d532592e3819332
LuckyOwlBen <benmalmberg2@gmail.com>
2019-07-08 11:59:11 -0400

    finished the challenge

---
+++ Week2CodingChallenge/src/main/java/com/revature/driver/Driver.java
@@ -0,0 +1,19 @@
+package com.revature.driver;
+
+import com.revature.DAOImpl.DAOUtility;
+import com.revature.beans.Employee;
+import com.revature.services.EmployeeList;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		DAOUtility.tryReturnAllDepartments();
+		DAOUtility.tryReturnAllEmployees();
+		EmployeeList.printAverageSalaryByDpt();
+		Employee e = new Employee("Barry","Blue",1,123455,"barryblue@gmail.com");
+		DAOUtility.tryaddNewEmployee(e);
+
+	}
+
+}
