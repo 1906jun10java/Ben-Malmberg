c1b56404c8680c285f829da01a7267326dec5fdb
LuckyOwlBen <benmalmberg2@gmail.com>
2019-07-18 20:25:42 -0400

    FULL VERTICAL

--- ExpenseReimbursementSystem/src/main/java/com/revature/services/Login.java
+++ ExpenseReimbursementSystem/src/main/java/com/revature/services/Login.java
@@ -4,7 +4,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 
 import com.revature.beans.Employee;
 import com.revature.daoimpl.EmployeeDAOImpl;
@@ -12,7 +11,7 @@ import com.revature.daoimpl.EmployeeDAOImpl;
 public class Login {
 	
 	EmployeeDAOImpl edi = new EmployeeDAOImpl();
-	private static Logger log = Logger.getRootLogger();
+	
 	
 	public boolean loginTest(String username, String password) {
 		List<Employee> loginVerify = new ArrayList<>();
@@ -20,25 +19,26 @@ public class Login {
 		try {
 			loginVerify.addAll(edi.returnAllEmployeesSQL());
 		} catch (SQLException e) {
-			log.error("A SQL exception occured when attempting to access the database to return all employees");
 			e.printStackTrace();
 			return false;
 		}catch(Exception e) {
-			log.error("An exception occured when attempting to access the database to return all employees");
 			e.printStackTrace();
 			return false;
 		}
 	
 	
 		for(Employee e :loginVerify) {
-			if(e.getUserName() == username) {
-				if(e.getPassword() == password) {
+			if(e.getUserName().equals(username)){ 
+				if(e.getPassword().equals(password)) {
 					return true;
 				}
 			}
 		}
 	
-		log.warn("Invalid Login occured for "+ username);
 		return false;
+	}
+
+	public Employee makeMeACookie(String username) {
+		
 	}
 }
