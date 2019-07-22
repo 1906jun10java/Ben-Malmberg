5509ae2a85a036178295a10df562fd1644ceed61
LuckyOwlBen <benmalmberg2@gmail.com>
2019-07-22 15:26:50 -0400

    PART 3

--- codeChallengeWeek5/src/app/heroes/heroes.component.ts
+++ codeChallengeWeek5/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from '../models/hero'
-import { HEROES } from '../models/mock-heroes';
+import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes', // by convention, selectors are named [module-name]-[component-name]
