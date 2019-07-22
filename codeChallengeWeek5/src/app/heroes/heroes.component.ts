eb2447bf46d6d5cb8d806ef79fa86faf5f2a2860
LuckyOwlBen <benmalmberg2@gmail.com>
2019-07-22 15:34:31 -0400

    cleaned it up

--- codeChallengeWeek5/src/app/heroes/heroes.component.ts
+++ codeChallengeWeek5/src/app/heroes/heroes.component.ts
@@ -4,7 +4,7 @@ import { Hero } from '../hero'
 import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: 'app-heroes', // by convention, selectors are named [module-name]-[component-name]
+  selector: 'app-heroes', 
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
